70775d3c00c4b4d3aa7662415e53fe0219558ee1
DanielPerezF <daniel.perezf24@gmail.com>
2023-09-29 18:04:33 +0200

    Initial commit

---
+++ double_degree_spending.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+password = st.secrets["password"]
+
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+st.write(df)
